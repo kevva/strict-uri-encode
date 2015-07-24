109a2a469e149a993fb978cef9c5579d436c5bf5
Jonathan Grimes <jsg2021@users.noreply.github.com>
2015-07-24 11:32:30 -0500

    drop the type check
    Let the type coercion make the value into a string.

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 'use strict';
 module.exports = function (str) {
-	var type = typeof str;
-	if (str != null && (type === 'object' || type === 'function')) {
-		throw new TypeError('Expected a string, or a primitive type. ');
-	}
-
 	return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
 		return '%' + c.charCodeAt(0).toString(16);
 	});
