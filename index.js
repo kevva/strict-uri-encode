cce6c7c833237ad29cfe5b8d2b88f462223ed5ca
Kevin Martensson <kevinmartensson@gmail.com>
2015-07-27 09:56:48 +0200

    Remove type check
    Fixes #1 and #2.

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict';
 module.exports = function (str) {
-	if (typeof str !== 'string' && typeof str !== 'number') {
-		throw new TypeError('Expected a string or a number');
-	}
-
 	return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
 		return '%' + c.charCodeAt(0).toString(16);
 	});
