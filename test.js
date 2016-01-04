915ec66b31ff5babafaa7caffab555a906f4d5c8
Kevin Martensson <kevinmartensson@gmail.com>
2016-01-04 18:12:49 +0100

    Update `ava`

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-'use strict';
-var test = require('ava');
-var strictUriEncode = require('./');
+import test from 'ava';
+import fn from './';
 
-test(function (t) {
-	t.assert(strictUriEncode('unicorn!foobar') === 'unicorn%21foobar');
-	t.assert(strictUriEncode('unicorn\'foobar') === 'unicorn%27foobar');
-	t.assert(strictUriEncode('unicorn*foobar') === 'unicorn%2Afoobar');
-	t.assert(strictUriEncode('unicorn*foobar') !== encodeURIComponent('unicorn*foobar'));
-	t.end();
+test(t => {
+	t.is(fn('unicorn!foobar'), 'unicorn%21foobar');
+	t.is(fn('unicorn\'foobar'), 'unicorn%27foobar');
+	t.is(fn('unicorn*foobar'), 'unicorn%2Afoobar');
+	t.not(fn('unicorn*foobar'), encodeURIComponent('unicorn*foobar'));
 });
